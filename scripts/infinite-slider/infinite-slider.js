96003b779290e2fb451e611d30d9cb59cb469ff3
Айдар Нуранов <bajanga07@gmail.com>
2020-10-04 13:55:47 +0600

    final version
    updated infinite slider so it's fully adaptive, fixed bugs

--- scripts/infinite-slider/infinite-slider.js
+++ scripts/infinite-slider/infinite-slider.js
@@ -1,7 +1,14 @@
-let sliderItems = document.querySelector('.feedback__slider');
-let prev = document.querySelector('.feedback__prev');
-let next = document.querySelector('.feedback__next');
-let numberList = document.querySelectorAll(".feedback__number");
+let infiniteSliders = document.querySelectorAll(".infinite__slider");
+
+function infWrap(infiniteSlider) {
+
+let sliderWrapper = infiniteSlider.querySelector('.infinite__wrapper');
+let wrapperWidth = sliderWrapper.clientWidth;
+let sliderItems = infiniteSlider.querySelector('.infinite__slides');
+sliderItems.style.left = -wrapperWidth + 'px';
+let prev = infiniteSlider.querySelector('.infinite__prev');
+let next = infiniteSlider.querySelector('.infinite__next');
+let numberList = infiniteSlider.querySelectorAll(".infinite__number");
 
 function sliderMove(items, prev, next) {
     let posX1 = 0;
@@ -9,13 +16,17 @@ function sliderMove(items, prev, next) {
     let posInitial;
     let posFinal;
     let threshold = 100;
-    let slides = document.querySelectorAll(".feedback__slide");
+    let slides = infiniteSlider.querySelectorAll(".infinite__slide");
+    slides.forEach(function (elem) {
+        elem.style.width = wrapperWidth + 'px';
+    });
     let slideSize = slides[0].offsetWidth;
     let index = 0;
     let allowShift = true;
     let cloneFirst = slides[0].cloneNode(true);
     let cloneLast = slides[slides.length - 1].cloneNode(true);
     //*copy elemnts
+
 
     items.appendChild(cloneFirst);
     items.insertBefore(cloneLast, slides[0]);
@@ -44,6 +55,21 @@ function sliderMove(items, prev, next) {
 
     items.addEventListener("transitionend", checkIndex);
     
+    window.addEventListener("resize", resizing);
+
+    function resizing() {
+        infiniteSlider.querySelectorAll(".infinite__slide").forEach(function (elem) {
+            elem.style.width = sliderWrapper.clientWidth + 'px';
+        });
+        slideSize = slides[0].offsetWidth;
+        resizeLeftChange();
+    }
+
+    function resizeLeftChange() {
+        let currentSlide = Number(infiniteSlider.querySelector(".infinite__number__active").innerHTML);
+        items.style.left = (-currentSlide * slideSize) + 'px';
+    }
+
     function dragStart(e) {
         e = e || window.event;
         e.preventDefault();
@@ -53,8 +79,8 @@ function sliderMove(items, prev, next) {
         }
         else {
             posX1 = e.clientX;
-            document.addEventListener("mouseup", dragEnd);
-            document.addEventListener("mousemove", dragAction);
+        document.addEventListener("mouseup", dragEnd);
+        document.addEventListener("mousemove", dragAction);
         }
     } 
     
@@ -105,16 +131,16 @@ function sliderMove(items, prev, next) {
                 index--;
             }
         };
-        allowShift = false;
+            allowShift = false;
     }
 
     function numberSlide(e) {
         e.preventDefault();
-        if  (e.target === document.querySelector(".feedback__number__active")) {
+        if  (e.target === infiniteSlider.querySelector(".infinite__number__active")) {
             return;
         }
         let newNumber = Number(e.target.innerHTML);
-        let oldNumber = Number(document.querySelector(".feedback__number__active").innerHTML);
+        let oldNumber = Number(infiniteSlider.querySelector(".infinite__number__active").innerHTML);
         let change = newNumber - oldNumber;
         items.classList.add("shifting");
         if (allowShift) {
@@ -126,16 +152,17 @@ function sliderMove(items, prev, next) {
     }
 
     function numberChange(plus) {
-        document.querySelector(".feedback__number__active").className = document.querySelector(".feedback__number__active").className.replace(" feedback__number__active", "");
+        infiniteSlider.querySelector(".infinite__number__active").className = infiniteSlider.querySelector(".infinite__number__active").className.replace(" infinite__number__active", "");
         if ((index + plus) % slides.length < 0) {
-            numberList[slides.length - 1].classList.add("feedback__number__active");
+            numberList[slides.length - 1].classList.add("infinite__number__active");
         }
         else {
-            numberList[(index + plus) % slides.length].classList.add("feedback__number__active");
+            numberList[(index + plus) % slides.length].classList.add("infinite__number__active");
         }
     }
 
     function checkIndex() {
+        // console.log(item.className);
         items.className = items.className.replace(" shifting", "");
         if (index === -1) {
             items.style.left = -(slides.length * slideSize) + "px";
@@ -150,3 +177,8 @@ function sliderMove(items, prev, next) {
 }
 
 sliderMove(sliderItems, prev, next);
+};
+
+for (let div of infiniteSliders) {
+    infWrap(div);
+}
